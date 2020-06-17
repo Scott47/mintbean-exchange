a6e69359d376c3b51422f4b13a71a23e832cfd4b
scottsilver <scottsilver47@gmail.com>
2020-06-17 13:09:32 -0500

    Updates

---
+++ precache-manifest.0b4c97027b566076d31741c8eef83a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d858df8fa52b9dc661455842b8a0763d",
+    "url": "/mintbean-exchange/index.html"
+  },
+  {
+    "revision": "b8aa83696b825fcbed10",
+    "url": "/mintbean-exchange/static/css/main.7fabcb14.chunk.css"
+  },
+  {
+    "revision": "ff5603a328628a48eeed",
+    "url": "/mintbean-exchange/static/js/2.a9ab2377.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mintbean-exchange/static/js/2.a9ab2377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8aa83696b825fcbed10",
+    "url": "/mintbean-exchange/static/js/main.807f013c.chunk.js"
+  },
+  {
+    "revision": "cd07497ffb0eb381c40e",
+    "url": "/mintbean-exchange/static/js/runtime-main.3245cbc8.js"
+  }
+]);
