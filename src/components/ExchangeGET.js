fa3edb56307968a534f5004533a9fc345186e72a
scottsilver <scottsilver47@gmail.com>
2020-06-17 19:47:20 -0500

    exchange rate data renders in a table

--- src/components/ExchangeGET.js
+++ src/components/ExchangeGET.js
@@ -1,48 +1,43 @@
 import React, { useEffect, useState } from "react";
-import ExchangeTable from "./ExchangeTable"
+import ExchangeTable from "./ExchangeTable";
 
 //Author: Scott Silver
 //Purpose: Display Currency associated with fetch.
 //Methods: GET Currency
 
-const ExchangeGET = props => {
-  const [myCurrency, setMyCurrency] = useState([]);
+const ExchangeGET = (props) => {
+  const [myCurrency, setMyCurrency] = useState({ conversion_rates: {} });
 
   const getMyCurrency = () => {
-    fetch(`https://v6.exchangerate-api.com/v6/0a2319c23c1859c60e2f4c47/latest/EUR`, {
-      method: "GET",
-    })
-      .then(response => response.json())
-      .then(data => {setMyCurrency(data)
-      })
-};
-useEffect(
-  () => {
-    setMyCurrency(getMyCurrency())
-    console.log(myCurrency);
-  }, []);
+    fetch(
+      `https://v6.exchangerate-api.com/v6/9976b2b1ce62fc782f97dd26/latest/EUR`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMyCurrency(data);
+      });
+  };
+
+  useEffect(
+    () => {
+       getMyCurrency();
+    }, []);
 
   return (
     <>
-
       <div className="myCurrency-Div">
-        {myCurrency > 0 ? (
+        {Object.keys(myCurrency.conversion_rates).length > 0 ? (
 
-          myCurrency.conversion_rates(myCurrency).forEach(currency => {
-            return (
-              <div
-                key={currency}
-                mycurrency={currency}
-                getMyCurrency={getMyCurrency}
-              />
-             )
-            })
+          <ExchangeTable currency={myCurrency.conversion_rates} />
         ) : (
           <p>You have no money</p>
         )}
       </div>
     </>
   );
-}
+};
 
 export default ExchangeGET;
--- src/components/ExchangeTable.js
+++ src/components/ExchangeTable.js
@@ -6,12 +6,24 @@ const ExchangeTable = (props) => {
   return (
     <>
     <Container>
-      {Object.conversion_rates(props.myCurrency).forEach(currency => {
-          console.log(currency)
-     return (<div className="report">{currency.conversion_rates}</div>
-     )
-      })
-    }
+        <Table dark-light size="sm" hover bordered className='report'>
+            <thead>
+            <tr className='report-head'>
+                <th>Country</th>
+                <th>Rate</th>
+            </tr>
+            </thead>
+            <tbody>
+        {Object.entries(props.currency).map(currency => {
+             return ( <tr className='report' key={currency[0]}>
+                <td>{currency[0]}</td>
+                <td>{currency[1]}</td>
+            </tr>
+             )
+            })
+        }
+         </tbody>
+         </Table>
     </Container>
     </>
   );
